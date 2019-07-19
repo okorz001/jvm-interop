c7f0008b51ac51e687b1dbab59ec246edc8656bb
Oscar Korz <okorz001@gmail.com>
2019-07-18 20:54:45 -0700

    Demonstrate Groovy annotations

--- apps/interop-groovy-app-groovy/src/main/groovy/interop/groovy/app/groovy/Main.groovy
+++ apps/interop-groovy-app-groovy/src/main/groovy/interop/groovy/app/groovy/Main.groovy
@@ -1,8 +1,31 @@
 package interop.groovy.app.groovy
 
 import interop.groovy.lib.GroovyClass
+import interop.groovy.lib.GroovyConstructorAnnotation
+import interop.groovy.lib.GroovyFieldAnnotation
+import interop.groovy.lib.GroovyMethodAnnotation
+import interop.groovy.lib.GroovyParameterAnnotation
+import interop.groovy.lib.GroovyTypeAnnotation
 
 final String LANG = "Groovy"
+
+@GroovyTypeAnnotation(LANG2)
+class UseGroovyAnnotations {
+    // LANG2 is a class variable with a literal value, so it's a constant expression
+    public static final String LANG2 = "Groovy"
+
+    // Must use access modifier to force a field instead of a property
+    @GroovyFieldAnnotation(LANG2)
+    public int field
+
+    @GroovyConstructorAnnotation(LANG2)
+    UseGroovyAnnotations() {}
+
+    @GroovyMethodAnnotation(LANG2)
+    def method() {}
+
+    def method(@GroovyParameterAnnotation(LANG2) param) {}
+}
 
 GroovyClass.staticProperty = LANG
 println("Using Groovy static property in ${GroovyClass.staticProperty}")
@@ -22,3 +45,8 @@ instance.extensionInstanceMethod(LANG)
 GroovyClass.implementedInterface({ LANG })
 GroovyClass.implementedTrait({ LANG })
 GroovyClass.extendedBaseClass({ LANG })
+GroovyClass.usedTypeAnnotation(new UseGroovyAnnotations())
+GroovyClass.usedConstructorAnnotation(new UseGroovyAnnotations())
+GroovyClass.usedMethodAnnotation(new UseGroovyAnnotations())
+GroovyClass.usedFieldAnnotation(new UseGroovyAnnotations())
+GroovyClass.usedParameterAnnotation(new UseGroovyAnnotations())
--- apps/interop-java-app-groovy/src/main/java/interop/java/app/groovy/Main.java
+++ apps/interop-java-app-groovy/src/main/java/interop/java/app/groovy/Main.java
@@ -2,11 +2,30 @@ package interop.java.app.groovy;
 
 import interop.groovy.lib.GroovyBaseClass;
 import interop.groovy.lib.GroovyClass;
+import interop.groovy.lib.GroovyConstructorAnnotation;
 import interop.groovy.lib.GroovyExtensionMethods;
+import interop.groovy.lib.GroovyFieldAnnotation;
+import interop.groovy.lib.GroovyMethodAnnotation;
+import interop.groovy.lib.GroovyParameterAnnotation;
 import interop.groovy.lib.GroovyStaticExtensionMethods;
+import interop.groovy.lib.GroovyTypeAnnotation;
 
 public class Main {
     private static final String LANG = "Java";
+
+    @GroovyTypeAnnotation(LANG)
+    public static class UseGroovyAnnotations {
+        @GroovyFieldAnnotation(LANG)
+        public int field;
+
+        @GroovyConstructorAnnotation(LANG)
+        public UseGroovyAnnotations() {}
+
+        @GroovyMethodAnnotation(LANG)
+        public void method() {}
+
+        public void method(@GroovyParameterAnnotation(LANG) int param) {}
+    }
 
     public static void main(String[] args) {
         GroovyClass.setStaticProperty(LANG);
@@ -33,5 +52,10 @@ public class Main {
                 return LANG;
             }
         });
+        GroovyClass.usedTypeAnnotation(new UseGroovyAnnotations());
+        GroovyClass.usedConstructorAnnotation(new UseGroovyAnnotations());
+        GroovyClass.usedMethodAnnotation(new UseGroovyAnnotations());
+        GroovyClass.usedFieldAnnotation(new UseGroovyAnnotations());
+        GroovyClass.usedParameterAnnotation(new UseGroovyAnnotations());
     }
 }
