f378f78be88d4e3511f0796a0377d480914524b9
Oscar Korz <okorz001@gmail.com>
2019-07-26 20:03:20 -0700

    Add Scala->Java app

---
+++ apps/interop-scala-app-java/src/main/scala/interop/scala/app/java/Main.scala
@@ -0,0 +1,43 @@
+package interop.scala.app.java
+
+import interop.java.lib.{JavaClass, JavaConstructorAnnotation, JavaFieldAnnotation, JavaLibrary, JavaMethodAnnotation, JavaParameterAnnotation, JavaTypeAnnotation}
+
+object Main extends App {
+  // The final is required here to make a constant expression. (???)
+  final val LANG = "Scala"
+
+  @JavaTypeAnnotation(LANG)
+  class UseJavaAnnotations @JavaConstructorAnnotation(LANG) () {
+    // This annotates the field, not the generated getter or setter.
+    // However, the backing field is always private, so it is not visible with getFields().
+    // Private fields are still visible with getDeclaredFields(), but this violates access control.
+    @JavaFieldAnnotation(LANG)
+    var field: Int = 0
+
+    @JavaMethodAnnotation(LANG)
+    def method() = {}
+
+    def method(@JavaParameterAnnotation(LANG) param: Int) = {}
+  }
+
+  JavaClass.staticField = LANG
+  println(s"Using Java static field in ${JavaClass.staticField}")
+  JavaClass.setStaticProperty(LANG)
+  println(s"Using Java static property in ${JavaClass.getStaticProperty}")
+  JavaClass.staticMethod(LANG)
+
+  val instance = new JavaClass
+  instance.instanceField = LANG
+  println(s"Using Java instance field in ${instance.instanceField}")
+  instance.setInstanceProperty(LANG)
+  println(s"Using Java instance property in ${instance.getInstanceProperty}")
+  instance.instanceMethod(LANG)
+
+  JavaLibrary.implementedInterface(() => LANG)
+  JavaLibrary.extendedBaseClass(() => LANG)
+  JavaLibrary.usedTypeAnnotation(new UseJavaAnnotations)
+  JavaLibrary.usedConstructorAnnotation(new UseJavaAnnotations)
+  JavaLibrary.usedMethodAnnotation(new UseJavaAnnotations)
+  //JavaLibrary.usedFieldAnnotation(new UseJavaAnnotations)
+  JavaLibrary.usedParameterAnnotation(new UseJavaAnnotations)
+}
