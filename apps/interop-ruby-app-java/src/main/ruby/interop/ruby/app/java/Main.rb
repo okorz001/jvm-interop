5bf431e2dbcf55e0d77a300f6a5b15737921beb7
Oscar Korz <okorz001@gmail.com>
2019-08-11 23:48:31 -0700

    More Ruby->Java features

--- apps/interop-ruby-app-java/src/main/ruby/interop/ruby/app/java/Main.rb
+++ apps/interop-ruby-app-java/src/main/ruby/interop/ruby/app/java/Main.rb
@@ -1,6 +1,10 @@
 java_package 'interop.ruby.app.java'
 
+java_import 'interop.java.lib.JavaBaseClass'
+java_import 'interop.java.lib.JavaCheckedException'
 java_import 'interop.java.lib.JavaClass'
+java_import 'interop.java.lib.JavaLibrary'
+java_import 'interop.java.lib.JavaUncheckedException'
 
 LANG = 'Ruby'
 
@@ -12,3 +16,27 @@ instance = JavaClass.new
 instance.instanceField = LANG
 puts "Used Java instance field in #{instance.instanceField}"
 instance.instanceMethod(LANG)
+
+JavaLibrary.varargs(LANG)
+JavaLibrary.varargs(LANG, 1)
+JavaLibrary.varargs(LANG, 1, 2)
+
+begin
+  JavaLibrary.throwsUnchecked()
+rescue JavaUncheckedException
+  puts "Caught Java unchecked exception in Ruby"
+end
+
+begin
+  JavaLibrary.throwsChecked()
+rescue JavaCheckedException
+  puts "Caught Java checked exception in Ruby"
+end
+
+JavaLibrary.implementedInterface { LANG }
+JavaLibrary.implementedInterfaceWithMethod { LANG }
+JavaLibrary.extendedBaseClass(Class.new(JavaBaseClass) do
+  def getLanguage
+    LANG
+  end
+end.new)
