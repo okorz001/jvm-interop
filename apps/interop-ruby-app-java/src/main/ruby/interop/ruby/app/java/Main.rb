b0b2c6c63c1ddbe7049391b951eb87824a02165d
Oscar Korz <okorz001@gmail.com>
2019-08-11 23:30:11 -0700

    Revert JRuby to 9.2.6.0
    String interpolation is throwing NPE in 9.2.7.0.

--- apps/interop-ruby-app-java/src/main/ruby/interop/ruby/app/java/Main.rb
+++ apps/interop-ruby-app-java/src/main/ruby/interop/ruby/app/java/Main.rb
@@ -4,4 +4,11 @@ java_import 'interop.java.lib.JavaClass'
 
 LANG = 'Ruby'
 
+JavaClass::staticField = LANG
+puts "Used Java static field in #{JavaClass::staticField}"
 JavaClass.staticMethod(LANG)
+
+instance = JavaClass.new
+instance.instanceField = LANG
+puts "Used Java instance field in #{instance.instanceField}"
+instance.instanceMethod(LANG)
